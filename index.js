612deeb8062646f91dc553561bd1b33d02571620
Radwan <muhammed.a.radwan@gmail.com>
2022-07-18 00:59:41 +0200

    create an enemy and a player

--- index.js
+++ index.js
@@ -5,3 +5,60 @@ canvas.width = 1024;
 canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 2;
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height)
+      this.velocity.y = 0;
+    else this.position.y += gravity;
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 200,
+    y: 200,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const enemy = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+  enemy.update();
+}
+animate();
