dac783a46eecaee55bfa78210c1d6c0f58f4cacf
Radwan <muhammed.a.radwan@gmail.com>
2022-07-27 21:45:40 +0200

    death animation

--- js/classes.js
+++ js/classes.js
@@ -100,6 +100,7 @@ class Fighter extends Sprite {
         this.isAttacking
         this.health = 100
         this.sprites = sprites
+        this.dead = false
 
         for (const sprite in this.sprites) {
             sprites[sprite].image = new Image()
@@ -109,7 +110,7 @@ class Fighter extends Sprite {
 
     update() {
         this.draw()
-        this.animateFrames()
+        if (!this.dead) this.animateFrames()
 
         // attack box
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
@@ -133,11 +134,20 @@ class Fighter extends Sprite {
     }
 
     takeHit() {
-        this.switchSprites('takeHit')
         this.health -= 20
+
+        if (this.health <= 0) this.switchSprites('death')
+        else this.switchSprites('takeHit')
     }
 
     switchSprites(sprite) {
+        // death image
+        if (this.image === this.sprites.death.image) {
+            if (this.framesCurrent === this.sprites.death.framesMax - 1)
+                this.dead = true
+            return
+        }
+
         // override animations on attack animation
         if (
             this.image === this.sprites.attack1.image &&
@@ -194,6 +204,13 @@ class Fighter extends Sprite {
                 if (this.image !== this.sprites.takeHit.image) {
                     this.image = this.sprites.takeHit.image
                     this.framesMax = this.sprites.takeHit.framesMax
+                    this.framesCurrent = 0
+                }
+                break
+            case 'death':
+                if (this.image !== this.sprites.death.image) {
+                    this.image = this.sprites.death.image
+                    this.framesMax = this.sprites.death.framesMax
                     this.framesCurrent = 0
                 }
                 break
